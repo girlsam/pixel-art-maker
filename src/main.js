2a7dc67ca49f423e22a663884f9a2f7743638d22
Sam Goldsmith <samggold@gmail.com>
2016-07-21 13:16:44 -0600

    push to gh pages

--- src/main.js
+++ src/main.js
@@ -1,31 +1,22 @@
 $(document).ready(function() {
-  console.log('sanity check');
-  for (var i=0; i < 5000; i++) {
-    $('body').append("<div class='square-box'></div>");
-  }
-  var color;
-  $(".selection").click(function () {
-      color = $(this).css("background-color");
-      $('#currentColor').css("background-color", color);
+for (var i=0; i < 1239; i++) {
+  $('.canvas').append("<div class='square-box'></div>");
+}
+var color;
+$(".selection").click(function () {
+    color = $(this).css("background-color");
+    $('#currentColor').css("background-color", color);
   });
-  // $('.square-box').hover(function() {
-  //   $(this).css('background-color', color);
-  // });
-  var isDown = false;   // Tracks status of mouse button
-  $('.square-box').mousedown(function() {
-      isDown = true; // When mouse goes down, set isDown to true
-    })
-   $('.square-box').mouseup(function() {
-      isDown = false;    // When mouse goes up, set isDown to false
-    });
-
-   $('.square-box').mouseover(function() {
-     if(isDown) {        // Only change css if mouse is down
-        $(this).css('background-color', color)
-     }
-    });
-
-  // $('.square-box').on('mouseover', function(){
-  //   $(this).css('background-color', 'red');
-  // });
+var isDown = false;
+$('.square-box').mousedown(function() {
+    isDown = true;
+$('.square-box').mouseup(function() {
+    isDown = false;
+  });
+$('.square-box').mouseover(function() {
+    if(isDown) {
+      $(this).css('background-color', color);
+    }
+  });
+});
 });
